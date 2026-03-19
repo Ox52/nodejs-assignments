1fec39ac47cde47bbfb2fa15c422b97227499195
Ox52 <harsh04241@gmail.com>
2026-03-19 14:54:11 +0530

    done authenticationserver

--- 01-nodejs/authenticationServer01.js
+++ 01-nodejs/authenticationServer01.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 
 const app = express();
 
@@ -6,6 +6,73 @@ const Port = 3000;
 
 app.use(express.json());
 
-var user = [];
+const users = [];
 
-app.post("Singup", (req, res) => {});
+app.post("/signup", (req, res) => {
+  const user = req.body;
+
+  const userAlredyExits = false;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === user.email) {
+      userAlredyExits = true;
+      break;
+    }
+  }
+
+  if (userAlredyExits) {
+    res.status(400).send("user already exits");
+  } else {
+    users.push(user);
+    res.status(200).send("signup succesfully");
+  }
+});
+
+app.post("/login", (req, res) => {
+  const user = req.body;
+
+  const userFound = null;
+
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].email === user.email && users[i].password === user.password) {
+      userFound = users[i];
+      break;
+    }
+  }
+
+  if (userFound) {
+    res.json({
+      firstName: userFound.firstName,
+      lastName: userFound.lastName,
+      email: userFound.email,
+    });
+  } else {
+    res.status(400);
+  }
+});
+
+app.post("/data", (req, res) => {
+  var email = req.headers.email;
+  var password = req.headers.password;
+
+  let userFound = false;
+
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].email === email && users[i].password === password) {
+      userFound = true;
+    }
+  }
+
+  if (userFound) {
+    const userToReturn = users.map((user) => ({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    }));
+    res.json({
+      users: userToReturn,
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
